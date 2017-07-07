cd086287341d7caa1ea98b955b690120021a24ac
jenitivecase <jbrussow@gmail.com>
2017-07-07 13:07:19 -0500

    fix possible inequality in files; iteratively create reports

--- master_report_template.R
+++ master_report_template.R
@@ -6,3 +6,24 @@ results_folder <- "./Results_20170630/"
 model_fnames <- grep("result", list.files(results_folder), value = TRUE)
 factor_items_fnames <- grep("items", list.files(results_folder), value = TRUE)
 
+if(length(model_fnames) != length(factor_items_fnames)){
+  model_fnames_subjs <- simplify2array(strsplit(model_fnames, "_"))[1,]
+  factor_items_fnames_subjs <- simplify2array(strsplit(factor_items_fnames, "_"))[1,]
+  
+  if(length(model_fnames) <= length(factor_items_fnames)){
+    factor_items_fnames <- factor_items_fnames[which(factor_items_fnames_subjs %in% model_fnames_subjs)]
+  } else if(length(model_fnames) >= length(factor_items_fnames)){
+    model_fnames <- model_fnames[which(model_fnames_subjs %in% factor_items_fnames_subjs)]
+  }
+  
+  rm(model_fnames_subjs, factor_items_fnames_subjs)
+}
+
+for(i in 1:length(model_fnames)){
+  model_subj <-unlist(strsplit(model_fnames[i], "_"))[1] 
+  rmarkdown::render("K:/AscendKC/Corp/R_and_D/1-USERS/Jennifer Brussow/Outcome Modeling/Outcome_analysis/Outcome_Item_Analysis_Results.Rmd",
+                    output_file = paste0(model_subj, "_Report_", date, ".pdf"),
+                    output_dir = "./Reports")
+  
+}
+
