a3235e862f505bf94f3ab0f021bbfbbf144e4a5f
jenitivecase <jbrussow@gmail.com>
2017-07-12 16:10:48 -0500

    reading in item outcome information

--- data_prep.R
+++ data_prep.R
@@ -4,3 +4,8 @@ outcomes_data <- read.table("item-level_RN-CP_outcome-scores.txt",
                             blank.lines.skip = TRUE)
 
 saveRDS(outcomes_data, "item-level_RN-CP_outcome-scores.rds")
+
+item_outcome_info <- read.table("item_outcome_info.txt",
+                                sep = ",", header = TRUE, as.is = TRUE,
+                                strip.white = TRUE, fill = TRUE,
+                                blank.lines.skip = TRUE)
