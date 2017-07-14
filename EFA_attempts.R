e7d31e1f38994a3e8942f18ba3be2733302493a5
jenitivecase <jbrussow@gmail.com>
2017-07-14 14:56:06 -0500

    struggling to destruggling to deal with missing data

--- EFA_attempts.R
+++ EFA_attempts.R
@@ -27,58 +27,81 @@ outcome_type_codes <- outcome_type_key[which(outcome_type_key$OutcomeTypeID %in%
 
 outcome_type_names <- outcome_type_key[which(outcome_type_key$OutcomeTypeID %in% outcome_type_codes), 
                                        "OutcomeTypeName"]
+
+
+
 # for(name in 1:length(outcome_type_names)){
 
+#restrict data to just one form to avoid missing data issues
+outcomes_data <- outcomes_data[which(outcomes_data$AssessmentID == 133086),]
+
 name  <- 1
-  code <- outcome_type_codes[name]
-  outcome <- outcome_type_names[name]
-  
-  subscore_codes <- outcome_code_key[which(outcome_code_key$OutcomeTypeID == code), 
-                                     c("OutcomeID_orig", "OutcomeID_map")]
-  subscore_names <- outcome_code_key[which(outcome_code_key$OutcomeTypeID == code), 
-                                     c("OutcomeDescription_orig", "OutcomeDescription_map")]
-  
-  data <- outcomes_data[which(outcomes_data$OutcomeTypeID == code), 
-                        c("BookletID", "qbtbQuestionID",  
-                          "IsCorrect", "OutcomeID", "OutcomeDescription")]
-  
-  gc()
-  
-  outcome_map <- unique(data[, c("qbtbQuestionID", "OutcomeID")])
-  
-  #eliminating extraneous columns before spreading data
-  data <- data[,c("BookletID", "qbtbQuestionID",  
-                  "IsCorrect")]
-  
-  #moving data into wide format for use with mirt
-  data <- data %>%
-    spread(qbtbQuestionID, IsCorrect)
-  
-  #identifying items with 0 variance in responses
-  bad_items <- NULL
-  
-  for(j in 2:ncol(data)){
-    if(sum(!is.na(unique(data[, j]))) < 2){
-      bad_items <- c(bad_items, colnames(data[j]))
-    }
+code <- outcome_type_codes[name]
+outcome <- outcome_type_names[name]
+
+subscore_codes <- outcome_code_key[which(outcome_code_key$OutcomeTypeID == code), 
+                                   c("OutcomeID_orig", "OutcomeID_map")]
+subscore_names <- outcome_code_key[which(outcome_code_key$OutcomeTypeID == code), 
+                                   c("OutcomeDescription_orig", "OutcomeDescription_map")]
+
+data <- outcomes_data[which(outcomes_data$OutcomeTypeID == code), 
+                      c("BookletID", "qbtbQuestionID",  
+                        "IsCorrect", "OutcomeID", "OutcomeDescription")]
+
+
+gc()
+
+outcome_map <- unique(data[, c("qbtbQuestionID", "OutcomeID")])
+
+#eliminating extraneous columns before spreading data
+data <- data[,c("BookletID", "qbtbQuestionID",  
+                "IsCorrect")]
+
+#moving data into wide format for use with mirt
+data <- data %>%
+  spread(qbtbQuestionID, IsCorrect)
+
+#identifying items with 0 variance in responses
+bad_items <- NULL
+
+for(j in 2:ncol(data)){
+  if(sum(!is.na(unique(data[, j]))) < 2){
+    bad_items <- c(bad_items, colnames(data[j]))
   }
-  
-  #removing those items from the item map
-  outcome_map <- outcome_map[which(!(outcome_map$qbtbQuestionID %in% bad_items)),]
-  
-  #preparing lists of the items loading onto each factor
-  n_factors <- length(unique(subscore_codes$OutcomeID_map))
-  
-  #run analysis
-  model_OLS <- psych::fa(r = data[, 2:ncol(data)], factors = n_factors, 
-                         missing = TRUE, impute = "median")
-  
-  model_ML_fa <- psych::fa(r = data[, 2:ncol(data)], factors = n_factors, 
-                         missing = TRUE, impute = "median",
-                         fm = "mle")
-  
-  model_ML_omega <- psych::omega(m = data[, 2:ncol(data)], factors = n_factors, fm = "mle")
-  
-  
-  gc()
+}
+
+#removing bad items from dataset
+data <- data[, !names(data) %in% bad_items]
+
+#removing those items from the item map
+outcome_map <- outcome_map[which(!(outcome_map$qbtbQuestionID %in% bad_items)),]
+
+#preparing lists of the items loading onto each factor
+n_factors <- length(unique(subscore_codes$OutcomeID_map))
+
+#run analysis
+model_OLS <- psych::fa(r = data[, 2:ncol(data)], factors = n_factors, 
+                       missing = TRUE, impute = "mean", cor = "tet")
+
+model_IRT_FA <- psych::irt.fa(r = data[, 2:ncol(data)], factors = n_factors, 
+                       missing = TRUE, impute = "mean", cor = "tet")
+
+model_ML_fa <- psych::fa(r = data[, 2:ncol(data)], factors = n_factors, 
+                       missing = TRUE, impute = "mean",
+                       fm = "mle", cor = "tet")
+
+model_ML_omega <- psych::omega(m = data[, 2:ncol(data)], factors = n_factors, fm = "mle")
+
+
+library(nFactors)
+
+cor(data[, 2:ncol(data)], use = "pairwise.complete.obs")
+
+ev <- eigen(cor(data[, 2:ncol(data)], use = "pairwise.complete.obs")) # get eigenvalues
+ap <- parallel(subject=nrow(data[, 2:ncol(data)]),var=ncol(data[, 2:ncol(data)]),
+               rep=100,cent=.05)
+nS <- nScree(x=ev$values, aparallel=ap$eigen$qevpea)
+plotnScree(nS)
+
+gc()
 # }
