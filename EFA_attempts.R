c3977720a5cbd47c3ed727e5d17193c7704c5c57
jenitivecase <jbrussow@gmail.com>
2017-07-17 11:51:52 -0500

    ggplot print

--- EFA_attempts.R
+++ EFA_attempts.R
@@ -116,12 +116,14 @@ for(name in 1:length(outcome_type_names)){
   # gridExtra::grid.table(factor_loadings)
   
   loadings_long <- gather(loadings, key = "Factor")
-  ggplot(loadings_long, aes(x = value, fill = Factor)) + 
+  load_plot <- ggplot(loadings_long, aes(x = value, fill = Factor)) + 
     geom_histogram(binwidth = 0.01, alpha = 0.7, position = "stack") + 
     labs(x = "Loading", y = "Count", 
          title = paste0("Factor Loadings for ", n_factors, "-factor Solution"),
          caption = paste0("Mean = ", round(mean(loadings_long$value), 3), 
                           ", SD = ", round(sd(loadings_long$value), 3)))
+  
+  load_plot
   
   #plot BICs
   x <- c(1:n_explore)
