ec1ce941ccb87802ff3758f5fbeb8663c634dedf
jenitivecase <jbrussow@gmail.com>
2017-07-26 11:36:59 -0500

    fixed results folder, year and dates push through

--- master_report_template.R
+++ master_report_template.R
@@ -2,7 +2,16 @@ source("K:/AscendKC/Corp/R_and_D/1-USERS/Jennifer Brussow/options.R")
 
 library(rmarkdown)
 
-results_folder <- "./Results_20170630/"
+out_folder <- paste0("Outcome_FLoad_Reports_", date)
+
+CP_year <- "2016"
+CP_forms <- "A-C"
+daterange1_1 <- "11/15/2016"
+daterange1_2 <- "12/31/2016"
+daterange2_1 <- "04/01/2017"
+daterange2_2 <- "05/15/2017"
+
+results_folder <- "./Results_20170725/"
 model_fnames <- grep("result", list.files(results_folder), value = TRUE)
 factor_items_fnames <- grep("items", list.files(results_folder), value = TRUE)
 
@@ -23,7 +32,7 @@ for(i in 1:length(model_fnames)){
   model_subj <-unlist(strsplit(model_fnames[i], "_"))[1] 
   rmarkdown::render("K:/AscendKC/Corp/R_and_D/1-USERS/Jennifer Brussow/Outcome Modeling/Outcome_analysis/Outcome_Item_Analysis_Results.Rmd",
                     output_file = paste0(model_subj, "_Report_", date, ".pdf"),
-                    output_dir = "./Reports")
+                    output_dir = out_folder)
   
 }
 
