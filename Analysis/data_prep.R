c6dc7d4bc541cef25eed074373a0e4a29dbf2d1d
jenitivecase <jbrussow@gmail.com>
2017-11-21 12:53:16 -0600

    dealing with TTR data

--- Analysis/data_prep.R
+++ Analysis/data_prep.R
@@ -11,3 +11,15 @@ item_outcome_info <- read.table("item_outcome_info.txt",
                                 sep = ",", header = TRUE, as.is = TRUE,
                                 strip.white = TRUE, fill = TRUE,
                                 blank.lines.skip = TRUE)
+
+
+# TTR_data <- read.table("item-level_RN-2016CP_TTR-scores.txt",
+#                             sep = ",", header = TRUE, as.is = TRUE,
+#                             strip.white = TRUE, fill = TRUE,
+#                             blank.lines.skip = TRUE)
+
+TTR_data <- read.csv("item-level_RN-2016CP_TTR-scores.csv")
+names(TTR_data)[1] <- gsub("ï..", "", names(TTR_data)[1])
+
+saveRDS(TTR_data, "item-level_RN-2016CP_TTR-scores.rds")
+gc()
